3a884f8059a4d20bad50d4424d2acba0cb5db208
RendhikaAditya <aditya0909.a3@gmail.com>
2021-03-27 21:44:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/salesman/activity/CariCostumerActivity.java
@@ -0,0 +1,108 @@
+package com.example.salesman.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.salesman.BuildConfig;
+import com.example.salesman.R;
+import com.example.salesman.databinding.ActivityCariCostumerBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class CariCostumerActivity extends AppCompatActivity {
+
+    private ActivityCariCostumerBinding binding;
+    AlertDialog alertDialog;
+    String link = BuildConfig.BASE_API;
+    ArrayList<String> cost = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCariCostumerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        AndroidNetworking.initialize(this);
+        alertDialog = new SpotsDialog.Builder().setContext(this).setMessage("Sedang Mengambil Data ....").setCancelable(false).build();
+
+        getCost();
+
+
+    }
+
+    private void getCost() {
+        alertDialog.show();
+        AndroidNetworking.get(link+"costumer")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+//                            Toast.makeText(CariCostumerActivity.this, "data dapat", Toast.LENGTH_SHORT).show();
+                            Log.d("data", "cote : " + response + " | ");
+                            JSONObject meta = response.getJSONObject("meta");
+                            if (meta.getString("code").equalsIgnoreCase("200")) {
+                                JSONArray d = response.getJSONArray("data");
+                                alertDialog.hide();
+                                Log.d("data", "yeay : ");
+                                for (int i = 0; i < d.length(); i++) {
+                                    JSONObject data = d.getJSONObject(i);
+                                    cost.add(data.getString("nama_costumer"));
+                                    Log.d("cost,", "item :" + cost.size());
+                                }
+                                setAdapter();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("eror", "code : " + anError);
+                        Toast.makeText(CariCostumerActivity.this, "Pastikan Prangkat Terhubung Ke Internet", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private void setAdapter() {
+        adapter = new ArrayAdapter<>(this,
+                R.layout.auto_complete_row, cost);
+        binding.namaCost.setAdapter(adapter);
+        binding.namaCost.setDropDownBackgroundResource(R.drawable.shape_ed);
+
+
+        Log.d("data", "hasil : " + cost);
+        binding.namaCost.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(CariCostumerActivity.this, binding.namaCost.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/salesman/activity/LoginActivity.java
@@ -0,0 +1,93 @@
+
+package com.example.salesman.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.salesman.BuildConfig;
+import com.example.salesman.databinding.ActivityLoginBinding;
+import com.example.salesman.util.PrefManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import dmax.dialog.SpotsDialog;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    PrefManager prefManager;
+    String link = BuildConfig.BASE_API;
+    AlertDialog alertDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        AndroidNetworking.initialize(this);
+        prefManager = new PrefManager(this);
+        alertDialog = new SpotsDialog.Builder().setContext(this).setMessage("Sedang Mencoba Masuk ....").setCancelable(false).build();
+
+        if (prefManager.getLoginStatus()){
+            Intent intent = new Intent(LoginActivity.this, CariCostumerActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLogin();
+            }
+        });
+
+    }
+
+    private void getLogin() {
+        alertDialog.show();
+        AndroidNetworking.post(link+"sales/login")
+                .addBodyParameter("username", binding.etUsername.getText().toString())
+                .addBodyParameter("password", binding.etPassword.getText().toString())
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        alertDialog.hide();
+                        Log.d("code","data : "+response);
+                        try {
+                            if (response.getString("code").equalsIgnoreCase("200")){
+                                JSONObject data = response.getJSONObject("data");
+                                prefManager.setIdUser(data.getString("id_sales"));
+                                prefManager.setNamaUser(data.getString("nama_sales"));
+                                prefManager.setLoginStatus(true);
+                                Intent intent = new Intent(LoginActivity.this, CariCostumerActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                                finish();
+                            }else {
+                                Toast.makeText(LoginActivity.this, response.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("code","eror : "+anError);
+                        Toast.makeText(LoginActivity.this, "Koneksi Bermasalah", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
